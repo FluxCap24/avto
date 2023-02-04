3d209cb80b26203b17227359fde6a9595fdaa765
fluxcap24 <vitaliy.katkov.2017@mail.ru>
2023-02-04 13:47:03 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<?php  
+$login = "12345"; // логин
+$password = "12345"
+
+
+?> 
+
+
+<!DOCTYPE html> 
+<html lang="ru"> 
+<head> 
+ <title></title> 
+ <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> 
+ <link rel="stylesheet" type="text/css" href="css/style.css"> 
+ <meta content="text/html; charset=utf-8"> 
+</head> 
+<body> 
+
+ 
+<div class="container mt-4"> 
+  <div class="row"> 
+   <div class="col"> 
+    <!-- Форма регистрации --> 
+  <h2>Форма регистрации</h2> 
+  <form action="" method="post"> 
+   <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"><br> 
+   <input type="password" class="form-control" name="password" id="password" placeholder="Введите пароль"><br> 
+   <button class="btn btn-success" name="do_signup" type="submit">Войти</button> 
+  </form> 
+   </div> 
+  </div>
